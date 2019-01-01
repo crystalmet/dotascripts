7fec89c4d0c59c351fd8a0198ab8403749ce7b7e
crystalmet <43094712+crystalmet@users.noreply.github.com>
2019-01-02 04:32:18 +0500

    Update and rename test.lua to Lasthit.lua

--- Lasthit.lua
+++ Lasthit.lua
@@ -1,34 +1,33 @@
-local TestScript = {}
-TestScript.optionEnable = Menu.AddOptionBool({"Utility"}, "TestScript", false)
-TestScript.font = Renderer.LoadFont("Tahoma", 18, Enum.FontWeight.BOLD)
+local Lasthit = {}
+Lasthit.OptionEnable = Menu.AddOptionBool("Lasthit", false)
+Lasthit.Font = Renderer.LoadFont("Tahoma", 18, Enum.FontWeight.BOLD)
 
 local myHero, myPlayer
 local x, y
 
-function TestScript.Init() 
+function Lasthit.Init() 
 	myHero = Heroes.GetLocal()
 	myPlayer = Players.GetLocal()
 	x, y = Renderer.GetScreenSize()
 	x = x * 0.59
 	y = y * 0.8426
-	y = y - 20
-	
+	y = y - 20	
 end
 
-function TestScript.OnGameStart()
-	TestScript.Init()
+function Lasthit.OnGameStart()
+	Lasthit.Init()
 end
 
-function TestScript.OnUpdate()
+function Lasthit.OnUpdate()
 	if not Menu.IsEnabled(TestScript.optionEnable) or not myHero then return end
 end
 
-function TestScript.OnDraw()
+function Lasthit.OnDraw()
 	if not Menu.IsEnabled(TestScript.optionEnable) or not myHero then return end
 	
 	Renderer.SetDrawColor(90, 255, 100)
 	Renderer.DrawText(TestScript.font, x, y, "Go fuck yourself!")
 end
 
-TestScript.Init()
-return TestScript
+Lasthit.Init()
+return Lasthit
