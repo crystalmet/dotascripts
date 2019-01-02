0cbdf2ff477988059af8808b071a998c3a603450
crystalmet <43094712+crystalmet@users.noreply.github.com>
2019-01-02 05:17:02 +0500

    Update Lasthit.lua

--- Lasthit.lua
+++ Lasthit.lua
@@ -24,23 +24,6 @@ function Lasthit.OnDraw()
 	
 	Renderer.SetDrawColor(90, 255, 100)
 	Renderer.DrawText(Lasthit.Font, x, y, "Something works")
-	
-	for heroent, EnemyHero in pairs(EnemyHeroes) do
-		if EnemyHero.Valid then
-			local origin = Entity.GetAbsOrigin(heroent)
-			local HBO = EnemyHero.HBO
-			origin:SetZ(origin:GetZ() + HBO)
-			local hx, hy = Renderer.WorldToScreen(origin)
-
-			if Renderer.IsOnScreen(hx, hy) then
-				local HP = ceil(EnemyHero.HP)
-				local MaxHP = ceil(EnemyHero.MaxHP)
-				local HPText = HP .. "/" .. MaxHP
-				Renderer.SetDrawColor(222, 222, 222, 255)
-				Renderer.DrawText(Lasthit.Font, hx + 20, hy + 20, HPText)
-			end
-		end
-      end
 end
 
 Lasthit.Init()
